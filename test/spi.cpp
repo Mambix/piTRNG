2c17afa05913d891b2e4097951d920b2b1b70ee3
MambiX Ltd <ledi.mambix@gmail.com>
2018-10-20 13:52:48 +0000

    changes...

--- test/spi.cpp
+++ test/spi.cpp
@@ -1,13 +1,31 @@
-#include <bcm2835.h>
+// Compile with: g++ -Wall spi.cpp ../src/SPIClass.cpp ../src/EntropyClass.cpp ../GPIO/GPIOClass.cpp -lbcm2835 -o testSPI
+
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../src/SPIClass.h"
 
 // g++ -Wall spi.cpp -lbcm2835 -o spi
 
 int main (void)
 {
-    while(1)
-    {
+    SPIClass* spi = new SPIClass();
 
+    spi->Start();
+    usleep(1000000);
+
+    char* data = spi->ReadManyTimes(100);
+    
+    cout << "Data:" << endl;
+    for (int i=0; i<100; i++) {
+	cout << *data << endl;
+	data++;
     }
+    delete[] data;
+
+    spi->Stop();
 
     return 0;
 }
